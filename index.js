af865eeba8da74295755932414d7da9e06938203
Kelsey Thang <thang.kelsey@gmail.com>
2022-06-02 23:52:37 -0400

    Updates to API

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // api config
-require('dotenv').config();
-const apiKey = process.env.API_KEY;
+const apiKey = config.API_KEY;
+
+fetch(`https://rawg.io/api/games?page_size=20?token&key=${apiKey}`)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error)); 
 
