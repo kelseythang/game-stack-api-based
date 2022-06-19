dcd841a4e1980c28a869cedb3223c9367fa6f4ec
Kelsey Thang <thang.kelsey@gmail.com>
2022-06-18 22:06:55 -0400

    Creates json server

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const cardContainer = document.querySelector('#card-container');
 // DOMContentLoaded fetch
 function fetchGames(){
 fetch(`https://rawg.io/api/games?page_size=20?token&key=${apiKey}`)
-  .then(res=> res.json())
+  .then(res => res.json())
   .then(data => {
-    console.log(data)
     cardContainer.innerHTML = '';
     renderCard(data);
   })
@@ -28,6 +27,8 @@ form.addEventListener('submit', event => {
   fetch(`https://rawg.io/api/games?search=${searchValue}token&key=${apiKey}`)
     .then(res => res.json())
     .then(data =>  renderCard(data));
+  
+  form.reset();
 });
   
 function renderCard(data){
@@ -46,6 +47,7 @@ function renderCard(data){
     cardRating.textContent = 'Rating: ' + game.rating;
     cardDetails.textContent = 'View Details'; 
     cardDetails.style.textDecoration = 'underline';
+    cardDetails.style.cursor = 'pointer';
     cardDetails.classList.add('details');
     cardAdd.textContent = 'Add to Collection';
     cardAdd.classList.add('button');
@@ -55,15 +57,31 @@ function renderCard(data){
     cardDiv.append(cardTitle, cardImg, cardRating, cardDetails, cardAdd);
     
     // add to collection event
-    cardAdd.addEventListener('click', () => {
+    cardAdd.addEventListener('click', (event) => {
       if(cardAdd.textContent === 'Add to Collection'){
         cardAdd.textContent = 'Added to Collection';
         cardAdd.classList.remove('button');
-        cardAdd.classList.add('added-button')
+        cardAdd.classList.add('added-button');
+        const configObj = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            'title': game.name,
+            'image': game.background_image,
+          }),
+        };
+      
+        fetch('http://localhost:3000/games/', configObj)
+          .then(res => res.json());
+
       } else {
         cardAdd.textContent = 'Add to Collection';
         cardAdd.classList.remove('added-button');
         cardAdd.classList.add('button');
+        handleDelete(event);
       };
     });
 
@@ -114,3 +132,56 @@ function renderCard(data){
 // home page event
 const home = document.querySelector('#home');
 home.addEventListener('click', fetchGames);
+
+// my collection event
+const myCollection = document.querySelector('#my-collection');
+myCollection.addEventListener('click', fetchMyGames);
+
+function fetchMyGames() {
+fetch(`http://localhost:3000/games/`)
+  .then(res => res.json())
+  .then(data => {
+    cardContainer.innerHTML = '';
+    data.map(games => renderMyCards(games))
+  })
+  .catch(error => console.log('error', error)); 
+} 
+
+function renderMyCards(games) {
+    const cardDiv = document.createElement('div');
+    const cardTitle = document.createElement('p');
+    const cardImg = document.createElement('img');
+    const deleteBtn = document.createElement('button');
+
+    cardTitle.textContent = games.title;
+    cardImg.src = games.image;
+    cardDiv.classList.add('cardDefault');
+    cardImg.classList.add('cardImg');
+    deleteBtn.textContent = 'Delete From Collection';
+    deleteBtn.classList.add('delete-button');
+    deleteBtn.addEventListener('click', handleDelete);
+
+    cardContainer.appendChild(cardDiv);
+    cardDiv.append(cardTitle, cardImg, deleteBtn);
+};
+
+function handleDelete(event) {
+  fetch(`http://localhost:3000/games/`) 
+    .then(res => res.json())
+    .then(data => {
+      const deleteSearch = event.target.parentElement.firstChild.textContent;
+
+      data.find(game => {
+        if(game.title === deleteSearch){
+          fetch(`http://localhost:3000/games/${game.id}`, 
+          {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+        };
+      });
+    });
+};
+
